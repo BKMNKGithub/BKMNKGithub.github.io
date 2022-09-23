26594841f891d8f40d75264d781f389de5fddfb0
BKMNKGithub <114132433+BKMNKGithub@users.noreply.github.com>
2022-09-23 15:18:19 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,21 @@ function scriptMain() {
   left_anon.style.top = 0;
   right_anon.style.position = 'fixed';
   left_anon.style.position = 'fixed';
+
+  let coding_image = document.getElementById('coding-image');
+  let site_works = document.getElementById('site-work');
+
+  window.addEventListener("resize", (event) => {
+    if (window.innerWidth < 1100)
+      coding_image.style.marginTop = ((site_works.clientHeight - coding_image.clientHeight) / 2) + "px";
+    else
+      coding_image.style.marginTop = 0;
+
+    right_anon.style.left = window.innerWidth - (414 / 2);
+    right_anon.style.top = 0;
+    left_anon.style.left = 0 - (414 / 2);
+    left_anon.style.top = 0;
+    right_anon.style.position = 'fixed';
+    left_anon.style.position = 'fixed';
+  });
 }
